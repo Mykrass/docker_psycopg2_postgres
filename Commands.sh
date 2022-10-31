243406510dd1da593cea628ce5db82adc4ef52ab
Igor Krasovskiy <mykras@me.com>
2022-10-31 17:33:37 +0100

    Create Commands.sh

---
+++ Commands.sh
@@ -0,0 +1,9 @@
+!cat docker-compose.yml
+
+!docker-compose -p dq_de up -d
+
+# check if postgres docker instance is alive
+!docker ps
+
+# retrieve the ip address of the container, seen from the host
+!docker inspect dq_de_db_1 | grep "IPAddress"
